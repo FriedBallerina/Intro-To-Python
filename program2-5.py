1227c1a076e8d0c043ea2be26d72fdd839d64d4f
FriedBallerina <FriedBallerina@users.noreply.github.com>
2021-01-14 20:39:33 +0000

    Add files via upload

---
+++ program2-5.py
@@ -0,0 +1,23 @@
+# category winds (mph) damage
+# 1 74-95 some
+# 2 96-110 extensive
+# 3 111-129 devastating
+# 4 130-156 catastrophic
+# 5 157+ catastrophic
+
+userAnswer = int(input("Please type in the wind speed to find out the potential damage: "))
+
+if userAnswer < 74:
+    print("You're fine.")
+elif userAnswer > 73 and userAnswer < 96:
+    print("Some damage.")
+elif userAnswer > 95 and userAnswer < 111:
+    print("Extensive damage.")
+elif userAnswer > 112 and userAnswer < 130:
+    print("Devastating damage.")
+elif userAnswer > 129 and userAnswer < 157:
+    print("Catastrophic damage.")
+elif userAnswer > 157:
+    print("Catastrophic damage.")
+else:
+    print("This is not a hurricane. Chill out.")
